16b33c79062eec0ea4e9ebb4736a760426dfacfb
Romesh K Tripathi <romeshktripathi@gmail.com>
2022-09-05 12:04:18 +0530

    5 september 2022 -> 12:04 PM

--- LinkedList/linkedlistds.cpp
+++ LinkedList/linkedlistds.cpp
@@ -1,89 +1,107 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-typedef struct node{
+typedef struct node
+{
     int data;
     struct node *next;
-    node(int x){
+    node(int x)
+    {
         data = x;
         next = nullptr;
     }
 } node;
-
-void createList(int x,node* &head){
-    node* tmp = new node(x);
-    node* ptr = head;
-    if(head == nullptr)
+// function to create List
+void createList(int x, node *&head)
+{
+    node *tmp = new node(x);
+    node *ptr = head;
+    if (head == nullptr)
     {
         head = tmp;
         return;
     }
-    while(ptr->next){
+    while (ptr->next)
+    {
         ptr = ptr->next;
     }
     ptr->next = tmp;
 }
-
-void printList(node* head){
-    node* ptr = head;
-    while(ptr){
-        cout<<ptr->data<<endl;
+// function to print list
+void printList(node *head)
+{
+    node *ptr = head;
+    while (ptr)
+    {
+        cout << ptr->data << endl;
         ptr = ptr->next;
     }
 }
-
-void deleteFirstNode(node* &head){
-    if(head){
+// function to delete first node of linked list
+void deleteFirstNode(node *&head)
+{
+    if (head)
+    {
         head = head->next;
     }
 }
-void deleteLastNode(node* &head){
-    node* tmp = head;
-    if(tmp == nullptr) return;
-    if(tmp->next == nullptr) {
+// function to delete last node of linked list
+void deleteLastNode(node *&head)
+{
+    node *tmp = head;
+    if (tmp == nullptr)
+        return;
+    if (tmp->next == nullptr)
+    {
         delete head;
         return;
     }
-    while(tmp->next->next){
+    while (tmp->next->next)
+    {
         tmp = tmp->next;
     }
-    node* deleteMe = tmp->next;
+    node *deleteMe = tmp->next;
     tmp->next = nullptr;
     delete deleteMe;
 }
 
-//function to insert a node at given position 
-//parameter-> data, position starting from 1, head node 
+// function to insert a node at given position
+// parameter-> data, position starting from 1, head node
 
-void insertAt(int data,int pos,node* &head){
-    node* temp = new node(data);
-        if(pos < 1) return ;
-        if(pos == 1) {
-            temp->next = head;
-            head = temp;
-            return;
-        }
-    node* ptr = head;
+void insertAt(int data, int pos, node *&head)
+{
+    node *temp = new node(data);
+    if (pos < 1)
+        return;
+    if (pos == 1)
+    {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+    node *ptr = head;
     int i = 1;
-    while(ptr->next and i<pos-1){
+    while (ptr->next and i < pos - 1)
+    {
         ptr = ptr->next;
         i++;
     }
     temp->next = ptr->next;
     ptr->next = temp;
-
-
 }
-//1->2->3->4->5
+// 1->2->3->4->5
 
-//function to search in a linked list
+// function to search in a linked list
 
-int searchList(int key,node* &head){
-    node* ptr = head;
+int searchList(int key, node *&head)
+{
+    node *ptr = head;
     int count = 0;
-    while(ptr){
+    while (ptr)
+    {
         count++;
-        if(ptr->data == key){
+        if (ptr->data == key)
+        {
             return count;
         }
         ptr = ptr->next;
@@ -91,39 +109,91 @@ int searchList(int key,node* &head){
     return -1;
 }
 
-//function to search in list recursively
-int searchListRecursive(int key,node* head){
-    if(head == nullptr) return -1;
-    if(head->data == key) return 1;
-    int res = searchListRecursive(key,head->next);
-    if(res == -1)
-    return -1;
-    else return res+1;
+// function to search in list recursively
+int searchListRecursive(int key, node *head)
+{
+    if (head == nullptr)
+        return -1;
+    if (head->data == key)
+        return 1;
+    int res = searchListRecursive(key, head->next);
+    if (res == -1)
+        return -1;
+    else
+        return res + 1;
+}
+// function to insert element in a sorted linked list
+// assume list is sorted in non-decreasing order.
+void insertInSortedList(int data, node *&head)
+{
+    node *temp = new node(data);
+    if (head == nullptr)
+    {
+        head = temp;
+        return;
+    }
+    node *ptr = head;
+    if(ptr->data > data){
+        temp->next = ptr;
+        head = temp;
+        return;
+    }
+    while(ptr->next and ptr->next->data < data){
+        ptr = ptr->next;
+    }
+    if(ptr->next == nullptr)
+        ptr->next = temp;
+    else
+        {
+            temp->next = ptr->next;
+            ptr->next = temp;
+        }
 }
 
-int main(){
-    int n,x;
-    node* head = nullptr;
-    cout<<"No of elements you want to enter";
-    cin>>n;
-    while(n--){
-        cin>>x;
-        createList(x,head);
+// function to find middle of the linked list
+void middleOfLinkedList(node *&head)
+{
+    if(head == nullptr)return;
+    node *slow = head;
+    node *fast = head;
+    while(fast->next and fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    cout<<"\nNow see your input "<<endl;
+    if(fast->next){
+        cout<<slow->next->data;
+    }
+    else cout<<slow->data;
+}
+//function to reverse linked list : iterative solution
+void reverseList(node *&head){
+    node *ptr = nullptr;
+    if(head == nullptr )return;
+    while(head->next){
+        node *temp = head;
+        head = head->next;
+        temp ->next = ptr;
+        ptr = temp;
+    }
+    head->next = ptr;
+}
+
+int main()
+{
+    int n, x;
+    node *head = nullptr;
+    cout << "No of elements you want to enter";
+    cin >> n;
+    while (n--)
+    {
+        cin >> x;
+        insertInSortedList(x, head);
+    }
+    cout << "\nNow see your input " << endl;
     printList(head);
-    deleteFirstNode(head);
-    cout<<"After deleting first node of your list:\n";
+    cout<<"\nMiddle of the linked list :  ";
+    middleOfLinkedList(head);
+    cout<<"\nReverse List\n";
+    reverseList(head);
     printList(head);
-    deleteLastNode(head);
-    cout<<"After deleting last node of your list:\n";
-    printList(head);
-    insertAt(6,1,head);
-    cout<<"After inserting at 3rd position\n";
-    printList(head);
-    cout<<"search in a list\nEnter key to search";
-    int key;
-    cin>>key;
-    cout<<searchListRecursive (key,head);
-    return 0;
 }
